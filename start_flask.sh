8578b3e71db33ced84e7ce3f5eab32a42e5e880a
bocaj88 <bocaj@michalski1.com>
2025-06-22 18:45:17 -0400

    Exporting display 0 will force it to show up on the dispaly regardless of where it was started. Win

--- start_flask.sh
+++ start_flask.sh
@@ -3,9 +3,14 @@
 
 # Initialize conda for bash shell
 eval "$(conda shell.bash hook)"
+# Set DISPLAY environment variable to use any available display
+# This is useful when running as a daemon or from SSH
+export DISPLAY=:0
 
 # Activate the conda environment
 conda activate kindle_fetcher
 
 # Run the Flask application
 python run_flask.py
+
+
